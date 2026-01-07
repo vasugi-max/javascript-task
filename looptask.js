48e195ce27294410b4c16accbbdb932366a5d955
vasugi-max <vasugikalimuthu27@gmail.com>
2026-01-07 10:57:02 +0530

    adding task

--- looptask.js
+++ looptask.js
@@ -1,11 +1,13 @@
-// Print Numbers: Write a program that prints numbers from 1 to 10 using a while loop.
-let i = 1;
+// // Print Numbers: Write a program that prints numbers from 1 to 10 using a while loop.
 
-while (i <= 10) {
-  console.log(i);
-  i++;
+
+let d = 1;
+
+while (d <= 10) {
+  console.log(d);
+  d++;
 }
-// Even Numbers: Print all even numbers from 2 to 20 using a while loop.
+// // Even Numbers: Print all even numbers from 2 to 20 using a while loop.
 let num = 2;
 
 while (num <= 20) {
@@ -13,14 +15,55 @@ while (num <= 20) {
   num += 2;
 }
 
-// Count of Numbers: Calculate the sum ofSum numbers from 1 to N using a while loop.
-let n = 10;   // You can change this value
+// // // Count of Numbers: Calculate the sum ofSum numbers from 1 to N using a while loop.
+
+let n = 10; 
 let sum = 0;
 let a = 1;
 
 while (a <= n) {
-    sum = sum + i;
-    i++;
+    sum = sum + a;
+    a++;
 }
 
+console.log("Sum =", sum);
+
+// // // Countdown Timer: Print numbers from 10 to 1 in reverse order.
+
 console.log("Sum of numbers from 1 to " + n + " is: " + sum);
+
+let b= 10;
+
+while (b >= 1) {
+    console.log(b);
+    b--;
+}
+// multiplication table program
+
+let hello= 7;   
+
+let i= 1;
+
+while (i <= 10) {
+    
+    console.log(hello+ " x " + i + " = " + (hello * i));
+    i++;
+}
+// Factorial Calculation using while loop
+
+let u = 5;      // given number
+let fact = 1;
+let l = 1;
+
+while (l <= num) {
+  fact = fact * l;
+  l++;
+}
+console.log("Factorial of " + num + " is " + fact);
+// Print Squares of Numbers (1 to 10)
+let c = 1;
+
+while (c <= 10) {
+  console.log(i + " square = " + (c * c));
+  c++;
+}
--- task.js
+++ task.js
@@ -40,3 +40,83 @@ let res = 10>12?"yes":"no"
 
 console.log(res);
 
+
+
+
+// 7/1/2025 mrng task
+
+// The electricity bill is calculated based on the number of units consumed
+
+let units = 250;
+let bill = 0;
+
+if (units <= 100) {
+    bill = units * 1;
+} 
+else if (units <= 200) {
+    bill = (100 * 1) + ((units - 100) * 2);
+} 
+else {
+    bill = (100 * 1) + (100 * 2) + ((units - 200) * 3);
+}
+// Different rates are applied for different unit slabs
+
+let units2 = 180;
+let bill2 = 0;
+
+if (units2 <= 100) {
+    bill2 = units2 * 1;
+}
+else if (units2 <= 200) {
+    bill2 = (100 * 1) + (units2 - 100) * 2;
+}
+else {
+    bill2 = (100 * 1) + (100 * 2) + (units2 - 200) * 3;
+}
+
+console.log("Units Consumed:", units2);
+console.log("Total Electricity Bill: ₹" + bill2);
+// Units from 0–100 are charged at ₹1.50 per unit
+let units1 = 180;
+let bill1 = 0;
+
+if (units1 <= 100) {
+    bill1 = units * 1.5;
+} else if (units1 <= 200) {
+    bill1 = 150 + (units1 - 100) * 2; 
+} else {
+    bill1 = 150 + 200 + (units1 - 200) * 3; 
+}
+console.log("Units Consumed:", units1);
+console.log("Total Bill: ₹" + bill1);
+// Units from 101–200 are charged at ₹2.50 per unit
+let unitsConsumed = 180; 
+
+let totalBill = 0;
+if (unitsConsumed > 200) {
+    totalBill = unitsConsumed * 3.5 - 250; 
+} else if (unitsConsumed > 100) {
+    totalBill = unitsConsumed * 2.5 - 100; 
+} else {
+    totalBill = unitsConsumed * 1.5;
+}
+console.log("Total Bill: ₹" + totalBill);
+// Units from 201–300 are charged at ₹4.00 per unit.
+let unitsConsumed3= 250;
+let totalBill = 0;
+if (unitsConsumed > 300) {
+    totalBill += (unitsConsumed - 300) * 5; 
+    unitsConsumed = 300;
+}
+if (unitsConsumed > 200) {
+    totalBill += (unitsConsumed - 200) * 4; 
+    unitsConsumed = 200;
+}
+if (unitsConsumed > 100) {
+    totalBill += (unitsConsumed - 100) * 2.5; 
+    unitsConsumed = 100;
+}
+totalBill += unitsConsumed * 1.5; 
+
+console.log("Total Bill: ₹" + totalBill);
+
